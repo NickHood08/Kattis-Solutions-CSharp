74179f37a561a70c94bba36255bd96ae7f355409
NickHood08 <nicholashood08@gmail.com>
2024-10-14 13:51:54 -0400

    Update tilhamingju.cs

--- Til hamingju/tilhamingju.cs
+++ Til hamingju/tilhamingju.cs
@@ -1,1 +1,9 @@
+using System;
 
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!");
+    }
+}
