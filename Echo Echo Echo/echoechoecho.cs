60e6d6a7c316e3e89f03a1d7bdc256a8b1e878ea
NickHood08 <nicholashood08@gmail.com>
2024-10-14 14:34:56 -0400

    Create echoechoecho.cs

---
+++ Echo Echo Echo/echoechoecho.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        Console.WriteLine(word + " " + word + " " + word);
+    }
+}
