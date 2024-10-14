38ece5375a1bea9a19077cdb0bbd7e26d97a89c6
NickHood08 <nicholashood08@gmail.com>
2024-10-14 14:09:29 -0400

    Create kvedja.cs

---
+++ Kveðja/kvedja.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class EmailSignature
+{
+    public static void Main(string[] args)
+    {
+        string sender = Console.ReadLine();
+        Console.WriteLine("Kvedja,\r\n" + sender);
+    }
+}
