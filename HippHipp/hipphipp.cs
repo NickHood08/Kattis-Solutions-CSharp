c1c19eead477758a2f720a07a87012a6e88b183c
NickHood08 <nicholashood08@gmail.com>
2024-10-14 13:49:52 -0400

    Update hipphipp.cs

--- HippHipp/hipphipp.cs
+++ HippHipp/hipphipp.cs
@@ -1,1 +1,12 @@
+using System;
 
+public class HippHipp
+{
+    public static void Main(string[] args)
+    {
+        for(int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("Hipp hipp hurra!");
+        }
+    }
+}
