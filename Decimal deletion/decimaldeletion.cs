4b19b833521b5a82d79dcb11bddf9a7d6f15c26e
NickHood08 <nicholashood08@gmail.com>
2024-10-14 16:21:39 -0400

    Create decimaldeletion.cs

---
+++ Decimal deletion/decimaldeletion.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        decimal num = Decimal.Parse(Console.ReadLine());
+        Console.WriteLine(Math.Round(num));
+    }
+}
