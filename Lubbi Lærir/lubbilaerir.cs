df949f7317974529dc0beb066ea8b829aa0e0e49
NickHood08 <nicholashood08@gmail.com>
2024-10-14 14:08:39 -0400

    Create lubbilaerir.cs

---
+++ Lubbi Lærir/lubbilaerir.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class LubbiLearning
+{
+    public static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        Console.WriteLine(word.Substring(0,1));
+    }
+}
