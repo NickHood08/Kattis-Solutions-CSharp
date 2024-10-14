eee872bec8b4bdfde6d4e316229c7e8d235cb116
NickHood08 <nicholashood08@gmail.com>
2024-10-14 14:14:40 -0400

    Create bergmal.cs

---
+++ BergMál/bergmal.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string keysLogged = Console.ReadLine();
+        Console.WriteLine(keysLogged);
+    }
+}
