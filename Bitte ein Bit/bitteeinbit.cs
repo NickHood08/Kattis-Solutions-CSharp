a051c2d29afe285bbba4efba290e23fc0455bd72
NickHood08 <nicholashood08@gmail.com>
2024-10-14 18:11:39 -0400

    Create bitteeinbit.cs

---
+++ Bitte ein Bit/bitteeinbit.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string bits = Console.ReadLine();
+        Console.WriteLine(bits.Substring(0,1));
+    }
+}
