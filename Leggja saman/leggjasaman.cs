a57cbc0082519293ab1621d5e93255ef7718b9f2
NickHood08 <nicholashood08@gmail.com>
2024-10-14 13:53:22 -0400

    Create leggjasaman.cs

---
+++ Leggja saman/leggjasaman.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int arnarCars = Int32.Parse(Console.ReadLine());
+        int hannesCars = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(arnarCars + hannesCars);
+    }
+}
