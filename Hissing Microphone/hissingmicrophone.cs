709fd36437b8dbffd29d7a5af7412e807b44f0c5
NickHood08 <nicholashood08@gmail.com>
2024-10-14 16:31:41 -0400

    Create hissingmicrophone.cs

---
+++ Hissing Microphone/hissingmicrophone.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string word = Console.ReadLine();
+        if(word.Contains("ss"))
+        {
+            Console.WriteLine("hiss");
+        }
+        else 
+        {
+            Console.WriteLine("no hiss");
+        }
+    }
+}
