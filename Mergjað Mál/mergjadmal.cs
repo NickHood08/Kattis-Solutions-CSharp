41d4b0b4e64981e0aa721a9759d36dca4df8f6e9
NickHood08 <nicholashood08@gmail.com>
2024-10-14 18:46:54 -0400

    Create mergjadmal.cs

---
+++ Mergjað Mál/mergjadmal.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        if(input.Contains("69") || input.Contains("420"))
+        {
+            Console.WriteLine("Mergjad!");
+        }
+        else
+        {
+            Console.WriteLine("Leim!");
+        }
+    }
+}
