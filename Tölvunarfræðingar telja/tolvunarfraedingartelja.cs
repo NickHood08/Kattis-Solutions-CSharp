3a79d7e1a09d0a9bb035b8371ef52c6a2e6b15a3
NickHood08 <nicholashood08@gmail.com>
2024-10-14 14:04:14 -0400

    Create tolvunarfraedingartelja.cs

---
+++ Tölvunarfræðingar telja/tolvunarfraedingartelja.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int input = Int32.Parse(Console.ReadLine());
+        Console.WriteLine(input - 1);
+    }
+}
