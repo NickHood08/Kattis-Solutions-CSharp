abe68d81d1dd8e901bad30dab0f324da395e4e39
NickHood08 <nicholashood08@gmail.com>
2024-10-14 16:26:57 -0400

    Create timeloop.cs

---
+++ Stuck In A Time Loop/timeloop.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        for(int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i + " Abracadabra");
+        }
+    }
+}
