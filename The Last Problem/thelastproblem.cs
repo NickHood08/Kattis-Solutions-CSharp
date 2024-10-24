53a168dcceb9d6e1b3faa9e31394fa8d2ea6835d
NickHood08 <nicholashood08@gmail.com>
2024-10-23 20:25:05 -0400

    Create thelastproblem.cs

---
+++ The Last Problem/thelastproblem.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine("Thank you, " + name + ", and farewell!");
+    }
+}
