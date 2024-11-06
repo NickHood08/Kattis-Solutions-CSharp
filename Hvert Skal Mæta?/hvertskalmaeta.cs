2d4cf60fb9340631377300e002478282d5ad44eb
NickHood08 <nicholashood08@gmail.com>
2024-11-06 18:36:00 -0500

    Create hvertskalmaeta.cs

---
+++ Hvert Skal Mæta?/hvertskalmaeta.cs
@@ -0,0 +1,49 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+      
+        switch(input)
+        {
+            case "Reykjavik":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Kopavogur":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Hafnarfjordur":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Reykjanesbaer":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Akureyri":
+                Console.WriteLine("Akureyri");
+                break;
+            case "Gardabaer":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Mosfellsbaer":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Arborg":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Akranes":
+                Console.WriteLine("Reykjavik");
+                break;
+            case "Fjardabyggd":
+                Console.WriteLine("Akureyri");
+                break;
+            case "Mulathing":
+                Console.WriteLine("Akureyri");
+                break;
+            case "Seltjarnarnes":
+                Console.WriteLine("Reykjavik");
+                break;
+        }
+    }
+}
