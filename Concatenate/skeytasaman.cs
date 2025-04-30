bd17195d77694843a335161034e1c56096b6313d
NickHood08 <nicholashood08@gmail.com>
2025-04-29 20:15:53 -0400

    Create skeytasaman.cs

---
+++ Concatenate/skeytasaman.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input1 = Console.ReadLine();
+        string input2 = Console.ReadLine();
+        string concat = input1 + input2;
+        Console.WriteLine(concat);
+    }
+}
