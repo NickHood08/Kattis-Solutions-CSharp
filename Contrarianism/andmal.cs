0c64373a2d0d405b49748001e96dae780d10c61f
NickHood08 <nicholashood08@gmail.com>
2025-04-29 20:30:29 -0400

    Create andmal.cs

---
+++ Contrarianism/andmal.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        
+        if(input != "ichi")
+        {
+            Console.WriteLine("ichi");
+        }
+        else
+        {
+            Console.WriteLine("ni");
+        }
+    }
+}
