8db02ace8338ecf5ee9b57cd20d66080a0a88ff0
NickHood08 <nicholashood08@gmail.com>
2025-06-11 14:31:59 -0400

    Create blackthorn.cs

---
+++ Blackthorn/blackthorn.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        
+        if(input.Contains("kth"))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
