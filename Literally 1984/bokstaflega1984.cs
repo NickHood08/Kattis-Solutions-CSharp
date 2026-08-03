74ab5e2ff4134fa42c8454b871ae1aaad413be5a
NickHood08 <nicholashood08@gmail.com>
2026-08-03 14:08:09 -0400

    Create bokstaflega1984.cs

---
+++ Literally 1984/bokstaflega1984.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        
+        if(input == "1984")
+        {
+            Console.WriteLine("Literally 1984");
+        }
+        else
+        {
+            Console.WriteLine("Not 1984... yet");
+        }
+    }
+}
